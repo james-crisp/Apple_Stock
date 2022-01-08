7c32ce0b9a6aa9e7d46223b12fc9bbc4550b871e
Jimmy Crisp <jimmycrisp@jimmys-air.lan>
2022-01-08 15:09:33 -0500

    Cannot get button push to work because button is missing type=button

--- testZipCode.py
+++ testZipCode.py
@@ -36,8 +36,10 @@ def check_available(url_check):
     options = Options()
     options.headless = True
     #driver = webdriver.Firefox(options=options)
+    #driver = webdriver.Firefox()
     driver = webdriver.Firefox()
-    
+    driver.implicitly_wait(0.5)
+    #driver.maximize_window()
     driver.get(url_check)
     #Click to change zipcode
     #wait = WebDriverWait(driver, 10)
@@ -69,11 +71,24 @@ def check_available(url_check):
     
     #<button class="rf-dude-quote-overlay-trigger as-delivery-overlay-trigger as-purchaseinfo-dudetrigger as-buttonlink" data-autom="deliveryDateChecker" data-ase-overlay="dude-overlay" data-ase-click="show">27514</button>
     
-    time.sleep(5)
     #button = driver.find_element_by_class_name("as-pruchaseinfo-dudeinfo-label")
-    button = driver.find_element_by_xpath('//div[@class="as-purchaseinfo-dudeinfo-dude2"]//span[@class="as-purchaseinfo-dudeinfo-suffixlabel"]//button[@class="rf-dude-quote-overlay-trigger as-delivery-overlay-trigger as-purchaseinfo-dudetrigger as-buttonlink"]')
-    button.click()
+    #button = driver.find_element_by_xpath('//div[@class="as-purchaseinfo-dudeinfo-dude2"]//span[@class="as-purchaseinfo-dudeinfo-suffixlabel"]//button[@class="rf-dude-quote-overlay-trigger as-delivery-overlay-trigger as-purchaseinfo-dudetrigger as-buttonlink"]')
+    #button.click()
     #//span[@class="as-purhcaseinfo-dudeinfo-label"]
+    
+    #wait = WebDriverWait(driver, 10000)
+    #age = wait.until(EC.visibility_of_element_located(button))
+    #age.click()
+    
+    #button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="as-purchaseinfo-dudeinfo-dude2"]//span[@class="as-purchaseinfo-dudeinfo-suffixlabel"]//button[@class="rf-dude-quote-overlay-trigger as-delivery-overlay-trigger as-purchaseinfo-dudetrigger as-buttonlink"]')))
+    #button.click()
+    
+    button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//button[@class="rf-dude-quote-overlay-trigger as-delivery-overlay-trigger as-purchaseinfo-dudetrigger as-buttonlink"]')))
+    button.submit()
+    
+   # <button type="button" class="rf-pickup-quote-overlay-trigger as-retailavailabilitytrigger-infobutton retail-availability-search-trigger as-buttonlink" data-ase-overlay="buac-overlay" data-ase-click="show">Apple Crabtree Valley Mall</button>
+    #<button class="rf-dude-quote-overlay-trigger as-delivery-overlay-trigger as-purchaseinfo-dudetrigger as-buttonlink" data-autom="deliveryDateChecker" data-ase-overlay="dude-overlay" data-ase-click="show">27601†† </button>
+    time.sleep(5)
     inputElement = driver.find_element_by_id("postalCode")
     inputElement.send_keys('27514')
     inputElement.submit()
