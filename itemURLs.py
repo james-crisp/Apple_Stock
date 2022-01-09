d53b052fc90cc36b471f3fbfd1455e9a66a9f2d1
Jimmy Crisp <jimmycrisp@jimmys-air.lan>
2022-01-09 10:32:37 -0500

    Document for the item URLs we are interested in

---
+++ itemURLs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  9 09:11:32 2022
+
+@author: jimmycrisp
+"""
+
+#Macbook Pro 14 Space Gray | M1 Pro 512 | M1 Pro 1TB
+url1 = "https://www.apple.com/shop/buy-mac/macbook-pro/14-inch-space-gray-8-core-cpu-14-core-gpu-512gb#"
+url2 = "https://www.apple.com/shop/buy-mac/macbook-pro/14-inch-space-gray-10-core-cpu-16-core-gpu-1tb#"
+
+#Macbook Pro 14 Silver | M1 Pro 512 | M1 Pro 1TB
+url3 = "https://www.apple.com/shop/buy-mac/macbook-pro/14-inch-silver-8-core-cpu-14-core-gpu-512gb#"
+url4 = "https://www.apple.com/shop/buy-mac/macbook-pro/14-inch-silver-10-core-cpu-16-core-gpu-1tb#"
+
+#Macbook Pro 16 Space Gray | M1 Pro 512, 1TB | M1 Max 1TB
+url5 = "https://www.apple.com/shop/buy-mac/macbook-pro/16-inch-space-gray-10-core-cpu-16-core-gpu-512gb#"
+url6 = "https://www.apple.com/shop/buy-mac/macbook-pro/16-inch-space-gray-10-core-cpu-16-core-gpu-1tb#"
+url7 = "https://www.apple.com/shop/buy-mac/macbook-pro/16-inch-space-gray-10-core-cpu-32-core-gpu-1tb#"
+
+#Macbook Pro 16 Silver | M1 Pro 512, 1TB | M1 Max 1TB
+url8 = "https://www.apple.com/shop/buy-mac/macbook-pro/16-inch-silver-10-core-cpu-16-core-gpu-512gb#"
+url9 = "https://www.apple.com/shop/buy-mac/macbook-pro/16-inch-silver-10-core-cpu-16-core-gpu-1tb#"
+url10 = "https://www.apple.com/shop/buy-mac/macbook-pro/16-inch-silver-10-core-cpu-32-core-gpu-1tb#"
+
+urls_array = [url1,url2,url3,url4,url5,url6,url7,url8,url9,url10]
+
+
